91171688ccbf6970ddaa9a0180f42de2c5c7f44c
thilak0805 <daya.22.thilak@gmail.com>
2026-01-16 16:31:56 +0530

    Flux-initial commit

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,4 +1,27 @@
 package com.learnreactiveprogramming.service;
 
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
 public class FluxAndMonoGeneratorService {
+
+    public Flux<String> namesFlux(){
+
+        return Flux.fromIterable(List.of("alex","ben","chloe")); // this list may be from db or remote service call
+
+    }
+    public static void main(String[] args) {
+
+        FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+
+        //only way to access this elements is to subscribe it, so we calling subscribe method
+
+        fluxAndMonoGeneratorService.namesFlux()
+                .subscribe(name ->{
+                    System.out.println("name is :"+name);
+                });
+
+    }
+
 }
