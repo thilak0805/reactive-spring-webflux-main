8278b0b447e860a8e97b19bc83135c4a72de924f
thilak0805 <daya.22.thilak@gmail.com>
2026-05-09 16:57:13 +0530

    zip,zipWith(Flux and Mono) with test cases

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -272,12 +272,55 @@ but for the request thats been made, there might be not data to return, in this 
         return aMono.mergeWith(bMono).log(); //A,B
     }
 
+    /*
+ 1. Used to combine two publishers (Flux) in to one.
+ 2. static method in flux
+ 3. Both the publishers are subscribed at the same time.(emitting the values at the same time).
+   - publishers are subscribed eagerly.
+ 4. Even though publishers are subscribed eagerly the merge happens in a sequence
+ */
+
     public Flux<String> explore_mergeSequential(){
         var abcFlux = Flux.just("A","B","C")
                 .delayElements(Duration.ofMillis(100)); //at the 100 ms, we have 'A' emitted, similarly at 200ms we have 'B' emitted
         var defFlux = Flux.just("D","E","F")
                 .delayElements(Duration.ofMillis(125)); // at the 125 ms we have 'D' emitted. similary at 250 second we have 'E' emitted
          return Flux.mergeSequential(abcFlux, defFlux).log(); // output is A,B,C,D,E,F
+    }
+
+
+    public Flux<String> explore_zip(){
+        var abcFlux = Flux.just("A","B","C");
+        var defFlux = Flux.just("D","E","F");
+
+        return Flux.zip(abcFlux, defFlux,
+                (first, second)->first+second).log() ;// output is AD,BE,CF
+    }
+
+    public Flux<String> explore_zip_1(){
+        var abcFlux = Flux.just("A","B","C");
+        var defFlux = Flux.just("D","E","F");
+        var _123Flux = Flux.just("1","2","3");
+        var _456Flux = Flux.just("4","5","6");
+
+        return Flux.zip(abcFlux, defFlux, _123Flux, _456Flux)
+                .map(t4 ->t4.getT1()+t4.getT2()+t4.getT3()+t4.getT4())
+                .log(); // output is AD,BE,CF
+    }
+
+    public Flux<String> explore_zipWith(){
+        var abcFlux = Flux.just("A","B","C");
+        var defFlux = Flux.just("D","E","F");
+
+        return abcFlux.zipWith(defFlux, (first,second)->first+second);// output is AD,BE,CF
+    }
+
+    public Mono<String> explore_mergeZipWith_mono(){
+        var aMono = Mono.just("A");
+        var bMono = Mono.just("B");
+        return aMono.zipWith(bMono)
+                .map(t2->t2.getT1()+t2.getT2())
+                .log(); // output : AB
     }
 
 
--- reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
+++ reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
@@ -236,4 +236,52 @@ class FluxAndMonoGeneratorServiceTest {
                 .expectNext("A","B","C","D","E","F")
                 .verifyComplete();
     }
+
+    @Test
+    void explore_zip() {
+        //given
+
+        //when
+        var value = fluxAndMonoGeneratorService.explore_zip();
+        //then
+        StepVerifier.create(value)
+                .expectNext("AD","BE","CF")
+                .verifyComplete();
+    }
+
+    @Test
+    void explore_zip_1() {
+        //given
+
+        //when
+        var value = fluxAndMonoGeneratorService.explore_zip_1();
+        //then
+        StepVerifier.create(value)
+                .expectNext("AD14","BE25","CF36")
+                .verifyComplete();
+    }
+
+    @Test
+    void explore_zipWith() {
+        //given
+
+        //when
+        var value = fluxAndMonoGeneratorService.explore_zipWith();
+        //then
+        StepVerifier.create(value)
+                .expectNext("AD","BE","CF")
+                .verifyComplete();
+    }
+
+
+    @Test
+    void explore_mergeZipWith_mono() {
+        //given
+        //when
+        var value = fluxAndMonoGeneratorService.explore_mergeZipWith_mono();
+        //then
+        StepVerifier.create(value)
+                .expectNext("AB")
+                .verifyComplete();
+    }
 }
